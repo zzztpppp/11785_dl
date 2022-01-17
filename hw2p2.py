edcc5ae55cee87db334fb1f77331fa580526a0c7
Azu <cxztpcom@gmail.com>
2022-01-17 17:55:37 +0800

    Add lr scheduler, add resnet50

--- hw2p2.py
+++ hw2p2.py
@@ -5,6 +5,7 @@ import torch.nn.functional as nf
 import numpy as np
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from sklearn.metrics import accuracy_score
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -64,6 +65,68 @@ class ResidualBlock(nn.Module):
         out = self.layers(x)
         shortcut = self.shortcut(x)
         return nf.relu(out + shortcut)
+
+
+class ResidualBlock3(nn.Module):
+    """
+    A resnet has 3 internal conv layers
+    """
+    def __init__(self, input_channels, output_channels, kernel_size, convolutional_shortcut=False):
+        super(ResidualBlock3, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels // 4, kernel_size=1, stride=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(output_channels // 4),
+            nn.Conv2d(output_channels // 4, output_channels // 4, kernel_size=kernel_size, stride=1,
+                      padding=(kernel_size - 1) // 2),
+            nn.ReLU(),
+            nn.BatchNorm2d(output_channels // 4),
+            nn.Conv2d(output_channels // 4, output_channels, kernel_size=1, stride=1)
+        )
+
+        if convolutional_shortcut:
+            self.shortcut = nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=1)
+        else:
+            self.shortcut = nn.Identity()
+
+    def forward(self, x):
+        out = self.layers(x)
+        shortcut = self.shortcut(x)
+        return nf.relu(out + shortcut)
+
+
+class ResNet50(nn.Module):
+    def __init__(self, n_classes):
+        super(ResNet50, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2),  # (64, 30, 30)
+            nn.MaxPool2d(3, 2),  # (64, 14, 14)
+            ResidualBlock3(64, 256, 3, True), # (256, 14, 14)
+            ResidualBlock3(256, 256, 3),  # (256, 14, 14)
+            ResidualBlock3(256, 256, 3),  # (256, 14, 14)
+            ResidualBlock3(256, 512, 3, True),  # (512, 14, 14)
+            ResidualBlock3(512, 512, 3),  # (512, 14, 14)
+            ResidualBlock3(512, 512, 3),  # (512, 14, 14)
+            ResidualBlock3(512, 512, 3),  # (512, 14, 14)
+            ResidualBlock3(512, 1024, 3, True),  # (1024, 14, 14)
+            ResidualBlock3(1024, 1024, 3),  # (1024, 14, 14)
+            ResidualBlock3(1024, 1024, 3),  # (1024, 14, 14)
+            ResidualBlock3(1024, 1024, 3),  # (1024, 14, 14)
+            ResidualBlock3(1024, 1024, 3),  # (1024, 14, 14)
+            ResidualBlock3(1024, 1024, 3),  # (1024, 14, 14)
+            ResidualBlock3(1024, 2048, 3, True),  # (2048, 14, 14)
+            ResidualBlock3(2048, 2048, 3),  # (2048, 14, 14)
+            ResidualBlock3(2048, 2048, 3),  # (2048, 14, 14)
+            nn.AdaptiveAvgPool2d((1, 1)),  #(2048, 1, 1)
+            nn.Flatten(),
+            nn.Linear(2048, 1000),
+            nn.ReLU(),
+            nn.BatchNorm1d(1000),
+            nn.Linear(1000, n_classes)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
 
 
 class ResNet18(nn.Module):
@@ -189,6 +252,7 @@ def train(training_set, val_set, model: nn.Module, criterion, n_epochs, lr, weig
     train_dataloader = DataLoader(training_set, batch_size=256, shuffle=True, pin_memory=True, num_workers=8)
     val_dataloader = DataLoader(val_set, batch_size=1024, shuffle=False, pin_memory=True, num_workers=8)
     optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=0.9, nesterov=True)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=1, cooldown=2)
     model.to(device)
     for e in range(n_epochs):
         model.train()
@@ -205,21 +269,35 @@ def train(training_set, val_set, model: nn.Module, criterion, n_epochs, lr, weig
 
             avg_loss += loss.item()
             if batch_num % 100 == 99:
-                print(f"Epoch: {e}, batch: {batch_num}, training_loss: {avg_loss / 10}")
+                print(f"Epoch: {e}, batch: {batch_num}, training_loss: {avg_loss / 100}")
                 avg_loss = 0.0
 
         # Validation per epoch
         model.eval()
         truth = []
         predictions = []
-        for batch_num, (x, y) in enumerate(val_dataloader):
-            x = x.to(device)
-            outputs = model(x).detach().cpu().numpy()
-            truth.append(y.numpy())
-            predictions.append(outputs.argmax(axis=1))
+        val_loss = 0.0
+        with torch.no_grad():
+            for batch_num, (x, y) in enumerate(val_dataloader):
+                x = x.to(device)
+                y = y.to(device)
+                outputs = model(x)
+                val_loss += (criterion(outputs, y).cpu().item() * len(y))
+                truth.append(y.cpu().numpy())
+                predictions.append(outputs.cpu().numpy().argmax(axis=1))
+        val_loss = val_loss / len(val_set)
+        scheduler.step(val_loss)
+        print(f"Validation loss at epoch {e}: {val_loss}")
         print(f"Validation accuracy at epoch {e}: {accuracy_score(np.concatenate(truth), np.concatenate(predictions))}")
 
     return model
+
+
+def eval(model, test_data):
+    model.eval()
+    with torch.no_grad():
+        pass
+
 
 
 def run_train():
@@ -227,9 +305,10 @@ def run_train():
     training_criterion = nn.CrossEntropyLoss()
     # model = ClassificationNetwork(3, n_classes, 32)
     # model = ConvNetClassifier1(n_classes)    # Best validation score 0.26
-    # model = ResNet18(n_classes)
-    model = ResNet34(n_classes)
-    n_epochs = 1000
+    # model = ResNet18(n_classes)    # Best validation score 0.6
+    # model = ResNet34(n_classes)    # Best validation score is 0.79 around epoch 20
+    model = ResNet50(n_classes)
+    n_epochs = 20
     trained_model = train(training_data, validation_data, model, training_criterion, n_epochs, lr=0.1, weight_decay=1e-3)
     torch.save(trained_model.state_dict(), "saved_model")
 
