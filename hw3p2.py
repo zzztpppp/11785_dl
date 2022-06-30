bfcfb2861047de2a1981812cfa8fa60cacfefb30
Azu <cxztpcom@gmail.com>
2022-06-30 15:38:56 +0800

    Down-sample the timestamp. Apply edit distance in validation.

--- hw3p2.py
+++ hw3p2.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import argparse
 import pandas as pd
+import Levenshtein
 from torch.nn.utils.rnn import pack_padded_sequence, pad_sequence, pad_packed_sequence
 from utils import greedy_search, beam_search_batch
 
@@ -103,7 +104,7 @@ class TestDataSet(Dataset):
         return pad_sequence(batch_x, batch_first=True), batch_seq_lengths
 
 
-training_batch_size = 64
+training_batch_size = 16
 val_batch_size = 128
 test_batch_size = 128  # Save memory for beam search
 
@@ -150,13 +151,15 @@ def pick_and_translate_beams(beam_results: torch.tensor, beam_scores: torch.tens
 class Model1(torch.nn.Module):
     def __init__(self, cnn_channels, n_lstm_layers, bidirectional, mlp_sizes, dropout, **kwargs):
         super(Model1, self).__init__()
-        self.cnn = torch.nn.Conv1d(in_channels=13, out_channels=cnn_channels, kernel_size=13, padding="same")
-        self.lstm = torch.nn.LSTM(input_size=cnn_channels, hidden_size=cnn_channels, num_layers=n_lstm_layers,
+        self.cnn_1 = torch.nn.Conv1d(in_channels=13, out_channels=cnn_channels, kernel_size=3, padding='same')
+        self.cnn_2 = torch.nn.Conv1d(in_channels=cnn_channels, out_channels=cnn_channels * 2, kernel_size=3,
+                                     stride=2)
+        self.lstm = torch.nn.LSTM(input_size=cnn_channels * 2, hidden_size=cnn_channels * 2, num_layers=n_lstm_layers,
                                   bidirectional=bidirectional, batch_first=True, dropout=dropout)
 
         # Use the last hidden output as the final lstm layer output
         factor = 2 if bidirectional else 1
-        mlp = [torch.nn.Linear(cnn_channels * factor, mlp_sizes[0]), torch.nn.ReLU(), torch.nn.Dropout(p=dropout)]
+        mlp = [torch.nn.Linear(cnn_channels * factor * 2, mlp_sizes[0]), torch.nn.ReLU(), torch.nn.Dropout(p=dropout)]
         for i in range(len(mlp_sizes) - 1):
             mlp.append(torch.nn.Linear(mlp_sizes[i], mlp_sizes[i + 1]))
             mlp.append(torch.nn.Dropout(p=dropout))
@@ -169,10 +172,14 @@ class Model1(torch.nn.Module):
     def forward(self, x, seq_lengths):
         # x is of shape (N, T, 13)
         # But conv1d requires (N, 13, T)
-        embed = self.cnn.forward(x.transpose(1, 2))
+        embed = self.cnn_1.forward(x.transpose(1, 2))
         embed = torch.nn.functional.relu(embed)
+        embed = self.cnn_2.forward(embed)
+        embed =  torch.nn.functional.relu(embed)
         # pack_padded_sequence requires (N, T, 13)
-        packed_embed = pack_padded_sequence(embed.transpose(1, 2), seq_lengths, batch_first=True, enforce_sorted=False)
+        # Since we are doning down-sampling ,the seq lengths change correspondingly.
+        down_sampled_seq_length = [(x - 3) // 2 for x in seq_lengths]
+        packed_embed = pack_padded_sequence(embed.transpose(1, 2), down_sampled_seq_length, batch_first=True, enforce_sorted=False)
 
         seq_out, _ = self.lstm.forward(packed_embed)  # (N, L, D*H_out)
         # Unpack
@@ -180,7 +187,14 @@ class Model1(torch.nn.Module):
         out = self.mlp.forward(unpacked_seq_out)
 
         # Shape of (N, L, 41)
-        return out
+        return out, down_sampled_seq_length
+
+
+def predict_string(model, batch_x, batch_seq_length, decoder):
+    result, _ = predict(model, batch_x, batch_seq_length)
+    beam_results, beam_scores, _, seq_len = decoder.decode(softmax(result, dim=-1))
+    best_seqs = pick_and_translate_beams(beam_results, beam_scores, seq_len)
+    return best_seqs
 
 
 def output_result(model, beam_width=10):
@@ -191,15 +205,13 @@ def output_result(model, beam_width=10):
     :param model:
     :return:
     """
-    output_seqs = []
-    model.to("cpu")
-    test_data_loader = get_unlabeled_data_loader(test_data_dir, test_data_order_dir, test_batch_size)
     decoder = CTCBeamDecoder(PHONEME_MAP, beam_width=beam_width, num_processes=8)
+    test_data_loader = get_unlabeled_data_loader(test_data_dir, test_data_order_dir, test_batch_size)
+    output_seqs = []
     for batch_x, batch_seq_length in test_data_loader:
-        result = predict(model, batch_x, batch_seq_length)
-        beam_results, beam_scores, _, seq_len = decoder.decode(softmax(result, dim=-1))
-        best_seqs = pick_and_translate_beams(beam_results, beam_scores, seq_len)
-        output_seqs.extend(best_seqs)
+        batch_string = predict_string(model, batch_x, batch_seq_length, decoder)
+        output_seqs.extend(batch_string)
+
     ids = np.arange(len(output_seqs))
     data = np.vstack([ids, np.array(output_seqs)])
     df = pd.DataFrame(data=data.T, columns=['id', 'predictions'])
@@ -210,11 +222,12 @@ def train_epoch(training_loader, model, criterion, optimizer):
     (batch_x, batch_y), (batch_seq_lengths, batch_target_sizes) = next(iter(training_loader))
     batch_x = batch_x.to(device)
     batch_y = batch_y.to(device)
+    model.to(device)
     model.train()
     optimizer.zero_grad()
-    outputs = model(batch_x, batch_seq_lengths)
+    outputs, new_seq_length = model(batch_x, batch_seq_lengths)
     # CTC loss requires (L B, C) so we transpose
-    loss = criterion(log_softmax(outputs.transpose(0, 1), dim=-1), batch_y, batch_seq_lengths, batch_target_sizes)
+    loss = criterion(log_softmax(outputs.transpose(0, 1), dim=-1), batch_y, new_seq_length, batch_target_sizes)
     print(f"Training loss {loss}")
     loss.backward()
     optimizer.step()
@@ -222,13 +235,15 @@ def train_epoch(training_loader, model, criterion, optimizer):
 
 def predict(model, x, x_lengths):
     model.eval()
-    y = model(x, x_lengths)
-    return y
+    y, new_length = model(x, x_lengths)
+    return y, new_length
 
 
 def validate(model, validation_loader, criterion, decoder=None):
     total_loss = 0.0
+    total_distance = 0.0
     total_size = 0
+    decoder = CTCBeamDecoder(PHONEME_MAP, beam_width=5, num_processes=8)
     with torch.no_grad():
         for batch_num, ((batch_x, batch_y), (batch_seq_lengths, batch_target_lengths)) in enumerate(validation_loader):
             batch_size, _ = batch_y.shape
@@ -236,19 +251,32 @@ def validate(model, validation_loader, criterion, decoder=None):
             batch_x = batch_x.to(device)
             batch_y = batch_y.to(device)
             # Output from model is (N, L, D) but ctcloss accepts (L, N, D)
-            y_hat = log_softmax(predict(model, batch_x, batch_seq_lengths), dim=-1)
-            loss = criterion(y_hat.transpose(0, 1), batch_y, batch_seq_lengths, batch_target_lengths)
+            logit, new_seq_length = predict(model, batch_x, batch_seq_lengths)
+            y_hat = log_softmax(logit, dim=-1)
+            loss = criterion(y_hat.transpose(0, 1), batch_y, new_seq_length, batch_target_lengths)
+            batch_string = predict_string(model, batch_x, batch_seq_lengths, decoder)
+            for y_hat_string, y in zip(batch_string, batch_y):
+                y_string = ''.join([PHONEME_MAP[c] for c in y])
+                total_distance += Levenshtein.distance(y_hat_string, y_string)
             total_loss += (loss.item() * batch_size)
 
     # Return average loss
-    return total_loss / total_size
+    return total_loss / total_size, total_distance / total_size
+
+
+def get_model(model_name, model_param):
+    if model_name == "Model1":
+        pass
 
 
 def train(model, optimizer_params):
-    n_epochs = 1000
+    n_epochs = 500
     print(model)
 
     lr = optimizer_params.get("lr")
+    warmup_epoch = optimizer_params.get("warmup")
+    if warmup_epoch is None:
+        warmup_epoch = 150
     weight_decay = optimizer_params.get("weight_decay")
     if lr is None:
         lr = 0.002
@@ -259,14 +287,14 @@ def train(model, optimizer_params):
     dev_loader = get_labeled_data_loader(dev_data_dir, dev_label_dir, val_batch_size)
     validation_decoder = CTCBeamDecoder(PHONEME_MAP, beam_width=beam_width)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=5, cooldown=0, verbose=True)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=10, cooldown=0, verbose=True)
     ctc_loss = torch.nn.CTCLoss()
-    model.to(device)
     for i in range(n_epochs):
         train_epoch(train_loader, model, ctc_loss, optimizer)
-        validation_loss = validate(model, dev_loader, ctc_loss)
-        print(f"Epoch {i}, validation_loss: {validation_loss}")
-        scheduler.step(validation_loss)
+        validation_loss, validation_distance = validate(model, dev_loader, ctc_loss)
+        print(f"Epoch {i}, validation_loss: {validation_loss}, validation_distance: {validation_distance}")
+        # Warmup for the first 150 epochs:
+        scheduler.step(validation_distance)
     return model
 
 
@@ -275,13 +303,14 @@ if __name__ == "__main__":
     parser.add_argument("mode", choices=['train', 'test'])
     parser.add_argument("model")
     parser.add_argument("--lr", type=float)
+    parser.add_argument("--warmup", type=int)
     parser.add_argument("--weight_decay", type=float)
     parser.add_argument("--model_path")
 
     args = parser.parse_args()
     training_params = vars(args)
     torch.cuda.empty_cache()
-    model_params = {"cnn_channels": 256, "n_lstm_layers": 4, "bidirectional": True, "mlp_sizes": [2048], "dropout": 0.3}
+    model_params = {"cnn_channels": 128, "n_lstm_layers": 4, "bidirectional": True, "mlp_sizes": [2048], "dropout": 0.3}
     model_params_str = f"({', '.join(['='.join([str(k), str(v)]) for k, v in model_params.items()])})"
     model_name = args.model
     training_model = eval(model_name + model_params_str)
