e4985e8033abe49c4aa68f11e61d2d6d6d81b04a
zutianpei <azu@fuyuncc.com>
2023-03-22 16:13:52 +0800

    Add argument to control lr step size

--- hw4p2_train.py
+++ hw4p2_train.py
@@ -305,6 +305,7 @@ if __name__ == "__main__":
     parser.add_argument("--training_batch_size", type=int, default=32)
     parser.add_argument("--validation_batch_size", type=int, default=512)
     parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--lr_step_size", type=int, default=1)
     parser.add_argument("--weight_decay", type=float, default=0.0)
     parser.add_argument("--embedding_size", type=int, default=512)
     parser.add_argument("--context_size", type=int, default=512)
